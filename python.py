96d6b25936a0c7e44bc60dd55004a644680c0257
Beling1 <43685205+Beling1@users.noreply.github.com>
2018-10-12 10:58:09 +0200

    Update python.py

--- python.py
+++ python.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template
 app=Flask(__name__, template_folder='.')
 
 @app.route("/")
-def index():
+def home():
     return render_template("home.html")
 	
 @app.route("/VBA")
